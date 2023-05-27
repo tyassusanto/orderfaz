7a8339b860fa96cdf028e7dbdc6231c44b4b2547
Adityas Susanto <92015831+tyassusanto@users.noreply.github.com>
2023-05-27 23:59:08 +0700

    minor change

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,8 +3,8 @@ const route = express.Router();
 const userController = require('../controller/users');
 const common = require('../common/helper')
 
-route.post('/register', userController.register) // done
-route.post('/login', userController.login) // done
-route.get('/profile', common.auth, userController.profile) // done
+route.post('/register', userController.register) 
+route.post('/login', userController.login) 
+route.get('/profile', common.auth, userController.profile) 
 
 module.exports = route
